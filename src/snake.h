7ca62c21538c4ac5f707eccd56e516fa14532d36
Panhakonpapa <linuxdev238@gmail.com>
2024-01-17 10:20:34 +0700

    Adding snake logic[not yet rendering done tmr] and wall deticiton:)

---
+++ src/snake.h
@@ -0,0 +1,43 @@
+//* Snaking Moudle for [bloppy] // 
+#include "food.h" 
+//* Defining Macro // 
+//
+#define HEIGHT 20
+#define WIDTH 20
+//* Trail for link list 
+//
+struct Tail {
+   int x, y; 
+}
+//* Poke Snake into memory // 
+struct Snake {
+     int x; 
+     int y; 
+     int direction; 
+     int HEIGHT; 
+     int WIDTH; 
+     int size;
+     struct Tail *tail; 
+}
+// Interllizing Snake [Bloppy] 
+//
+void snakeSetup(struct Snake *snake, struct Food *food) {
+	snake->x = 100; 
+	snake->y = 100;
+	snake->direction = 0; 
+	snake->HEIGHT = HEIGHT; 
+	snake->WIDTH = WIDTH; 
+	snake->size = 0; 
+	snake->tail = malloc(sizeof(struct Tail)); 
+}
+// Snake wall colition deticion 
+//
+int snakeHitwall(struct Snake *snake) {
+     if (snake->x < 0 || snake->y < 0) {
+             return 0; 
+     }
+     if (snake->x > 490 || snake->y > 490) {
+             return 0; 
+     }
+     return 1; 
+}
