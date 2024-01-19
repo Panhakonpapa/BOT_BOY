316784603eb37c354a3d179560462f821464cd7b
Panhakonpapa <linuxdev238@gmail.com>
2024-01-19 13:12:12 +0700

    Working player type

--- main.c
+++ main.c
@@ -9,30 +9,20 @@
 #define DELAY 400
 SDL_Window *window;
 SDL_Renderer *renderer; 
-struct Tail {
-   int x; 
-   int y; 
-};
-struct Snake {
-     int x; 
-     int y; 
-     int direction; 
-     int size; 
-     int width; 
-     int height; 
-}; 
-void renderSnake(struct Snake *snake, SDL_Renderer *renderer) {
-       snake->x = 400; 
-       snake->y = 400; 
-       snake->direction = 0; 
-       snake->size = 0; 
-       snake->width = WIDTH_SNAKE; 
-       snake->height = HEIGHT_SNAKE;
-       SDL_Rect sn = {snake->x, snake->y, snake->width, snake->height}; 
-       SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); 
-       SDL_RenderFillRect(renderer, &sn); 
-       SDL_RenderDrawRect(renderer, &sn);
-       SDL_RenderPresent(renderer); // Update the rendering object  
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+} Snake;
+void snakeMovement(Snake *snake, int dx, int dy) {
+    snake->x += dx;
+    snake->y += dy;
+}
+
+Snake Createsnake(int x, int y, int width, int height) {
+    Snake snake = {x, y, width, height};
+    return snake;
 }
 int main() { 
     printf("Initing the SDL_INTI SYSTEM...\n");  
@@ -60,29 +50,37 @@ int main() {
 	           -1, // set flags to -1  
 		   SDL_RENDERER_ACCELERATED // using graphic to drawing window
 		   ); 
-     
-     struct Snake snake; 
-     
+     Snake snake = Createsnake(100, 100, 50, 50);  
      SDL_Event event; 
      while (true) {
         // Main event Hendling Here // 	
 	if (SDL_PollEvent(&event) < 0) {
 		if (event.type == SDL_QUIT) 
-			break; 	
-	}
-	renderSnake(&snake, renderer); 
-	// Game play and Renderign gose here // 
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	// 
-	//
+			break; 
+	}	
+	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+        if (currentKeyStates[SDL_SCANCODE_UP]) {
+            snakeMovement(&snake, 0, -10);
+        }
+        if (currentKeyStates[SDL_SCANCODE_DOWN]) {
+            snakeMovement(&snake, 0, 10);
+        }
+        if (currentKeyStates[SDL_SCANCODE_LEFT]) {
+            snakeMovement(&snake, -10, 0);
+        }
+        if (currentKeyStates[SDL_SCANCODE_RIGHT]) {
+            snakeMovement(&snake, 10, 0);
+        }  
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set the screen to blak 
-	SDL_RenderClear(renderer); // Render and clear screen  
+	SDL_RenderClear(renderer); // Render and clear screen  		
+
+
+	// Game play and Renderign gose here // 
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_Rect drawSnake = { snake.x, snake.y, snake.width, snake.height };
+        SDL_RenderFillRect(renderer, &drawSnake);	
+
+	SDL_Delay(120);
 	SDL_RenderPresent(renderer); // Update the rendering object  
      }
      SDL_DestroyWindow(window); 
